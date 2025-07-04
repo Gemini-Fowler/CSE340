ee7a0f6117b24ce9d579441eb68d80eb6f569674
Jonas Crothers <jonascrothers@gmail.com>
2025-07-04 13:58:20 -0700

    i hope i did this correct

--- server.js
+++ server.js
@@ -1,30 +1,39 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
- * Require Statements
- *************************/
+/* ***************************************
+ * This is the application server
+ * ***************************************/
 const express = require("express")
-const env = require("dotenv").config()
+
 const app = express()
-const static = require("./routes/static")
+
+const expressLayouts = require("express-ejs-layouts")
+
+/* ******************************************
+ * View Engine and Templates
+ * ***************************************** */
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ******************************************
+ * Default GET route
+ * ***************************************** */
+app.get("/", (req, res) => { res.send("Welcome home!") })
+
+// Route to deliver the index view
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"});
+});
+
+
+/* ******************************************
+ * Server host name and port
+ * ***************************************** */
+const HOST = 'localhost'
+const PORT = 3000
 
 /* ***********************
- * Routes
- *************************/
-app.use(static)
-
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
-
-/* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
+* Log statement to confirm server operation
+* *********************** */
+app.listen(PORT, () => {
+    console.log(`trial app listening on ${HOST}:${PORT}`)
 })
