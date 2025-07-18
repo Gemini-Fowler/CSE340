7ae541941a4318c8968bc9d5b6ba27515691dcda
Jonas Crothers <jonascrothers@gmail.com>
2025-07-17 21:58:37 -0700

    edits

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const baseController = require("./controllers/baseController")
+const utilities = require("./utilities");
 
 const app = express()
 
@@ -18,6 +19,25 @@ app.use(express.static('public'));
 
 // Route to deliver the index view
 app.get("/", baseController.buildHome);
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ******************************************
  * Server host name and port
