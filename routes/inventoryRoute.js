d95c3257d7e9af4a24cb3472fa4cecd8812cfc77
Jonas Crothers <jonascrothers@gmail.com>
2025-11-14 18:59:23 -0800

    jdfh

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,20 +6,20 @@ const utilities = require("../utilities/")
 
 // Route to build inventory by classification view
 router.get(
-	"/type/:classificationId",
-	utilities.handleErrors(invController.buildByClassificationId)
+    "/type/:classificationId",
+    utilities.handleErrors(invController.buildByClassificationId)
 )
 
 // Route to build inventory detail view
 router.get(
-	"/detail/:invId",
-	utilities.handleErrors(invController.buildByInvId)
+    "/detail/:invId",
+    utilities.handleErrors(invController.buildByInvId)
 )
 
 // Route to intentionally trigger a server error (for testing)
 router.get(
-	"/trigger-error",
-	utilities.handleErrors(invController.throwError)
+    "/trigger-error",
+    utilities.handleErrors(invController.throwError)
 )
 
 module.exports = router
